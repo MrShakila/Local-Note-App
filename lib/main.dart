b78f1745db0187a43ff61bf91339b340c55f4e78
MrShakila <dscshakila@gmail.com>
2022-07-07 21:54:59 +0530

    add dekstop support

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:local_databse/providers/note_provider.dart';
 import 'package:local_databse/screen/home_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
+   if (Platform.isWindows || Platform.isLinux) {
+    // Initialize FFI
+    sqfliteFfiInit();
+    // Change the default factory
+    databaseFactory = databaseFactoryFfi;
+  }
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(
       create: (context) => NoteProvider(),
